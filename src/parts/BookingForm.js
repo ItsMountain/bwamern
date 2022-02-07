298ee8353ec0c6a295f61f3e63cf1f14faea3ae3
Mountain-Game <56752912+ItsMountain@users.noreply.github.com>
2022-02-07 08:27:55 +0700

    ㅤ

--- src/parts/BookingForm.js
+++ src/parts/BookingForm.js
@@ -114,7 +114,7 @@ class BookingForm extends Component {
           </span>{" "}
           per{" "}
           <span className="text-gray-900">
-            {data.duration} {itemDetails.unit}
+            {data.duration} {itemDetails.unit}{data.duration > 1 ? "s" : ""}
           </span>
         </h6>
 
